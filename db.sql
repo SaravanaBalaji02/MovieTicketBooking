5bad851f39f5c0820eb66a98c31b4706929459b4
Saravana Balaji A J <99321740+SaravanaBalaji02@users.noreply.github.com>
2024-07-28 19:54:53 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,29 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 18, 2020 at 07:10 PM
--- Server version: 5.1.53
--- PHP Version: 5.3.4
+
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
---
--- Database: `db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `register`
---
 
 CREATE TABLE IF NOT EXISTS `register` (
   `name` varchar(30) NOT NULL,
@@ -31,18 +11,14 @@ CREATE TABLE IF NOT EXISTS `register` (
   `password` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `register`
---
+
 
 INSERT INTO `register` (`name`, `email`, `password`) VALUES
-('pratap', 'pratap12@gmail.com', 'pratap');
+('Balaji', 'balaji@gmail.com', 'balu123');
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `table3`
---
+
 
 CREATE TABLE IF NOT EXISTS `table3` (
   `theatre` varchar(30) NOT NULL,
@@ -50,27 +26,24 @@ CREATE TABLE IF NOT EXISTS `table3` (
   `tickets` int(5) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `table3`
---
 
 INSERT INTO `table3` (`theatre`, `shows`, `tickets`) VALUES
-('srivishnu', '11:00AM', 250),
-('srivishnu', '7:00AM', 296),
-('asscars', '9:00PM', 250),
+('IMAX', '11:00AM', 250),
+('IMAX', '7:00AM', 296),
+('ELLORA', '9:00PM', 250),
 ('galaxy', '7:00AM', 250),
 ('PVR', '7:00AM', 255),
-('srivishnu', '9:00PM', 300),
-('srivishnu', '2:00PM', 260),
-('srivishnu', '6:00PM', 250),
-('asscars', '6:00PM', 350),
-('asscars', '2:00PM', 241),
-('asscars', '11:00AM', 300),
+('IMAX', '9:00PM', 300),
+('IMAX', '2:00PM', 260),
+('IMAAX', '6:00PM', 250),
+('ELLORA', '6:00PM', 350),
+('ELLORA', '2:00PM', 241),
+('ELLORA', '11:00AM', 300),
 ('galaxy', '11:00AM', 300),
 ('galaxy', '2:00PM', 250),
 ('galaxy', '6:00PM', 350),
 ('galaxy', '9:00PM', 250),
-('asscars', '7:00AM', 260),
+('ELLORA', '7:00AM', 260),
 ('PVR', '11:00AM', 300),
 ('PVR', '2:00PM', 250),
 ('PVR', '6:00PM', 350),
